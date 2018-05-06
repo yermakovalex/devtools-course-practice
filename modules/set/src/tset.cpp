c582360b4a4d0822da692fc896e11d37f44e87c5
Trintal <iliyalev@yandex.ru>
2018-05-06 21:18:00 +0300

    .

--- modules/set/src/tset.cpp
+++ modules/set/src/tset.cpp
@@ -21,39 +21,10 @@ void TSet::InsElem(const int Elem)
 {
 }
 
-TSet& TSet::operator=(const TSet &s)
-{
-}
-
 int TSet::operator==(const TSet &s) const
 {
     return 0;
 }
-
-int TSet::operator!=(const TSet &s) const
-{
-}
-
-TSet TSet::operator+(const TSet &s)
-{
-}
-
-TSet TSet::operator+(const int Elem)
-{
-}
-
-TSet TSet::operator-(const int Elem)
-{
-}
-
-TSet TSet::operator*(const TSet &s)
-{
-}
-
-TSet TSet::operator~(void)
-{
-}
-
 
 istream &operator>>(istream &istr, TSet &s)
 {
