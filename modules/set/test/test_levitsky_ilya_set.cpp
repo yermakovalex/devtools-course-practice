ab19ab2e0c00f717388c4fa8c91d08245323d6ef
Trintal <iliyalev@yandex.ru>
2018-05-15 19:24:31 +0300

    last try

--- modules/set/test/test_levitsky_ilya_set.cpp
+++ modules/set/test/test_levitsky_ilya_set.cpp
@@ -5,76 +5,76 @@
 #include "include/set.h"
 
 TEST(Levitsky_Ilya_setTest, Can_Create_Set) {
-  // Arrange
-  set A;
+    // Arrange
+    set A;
 
-  // Act
-  A.Insert(5);
+    // Act
+    A.Insert(5);
 
-  // Assert
-  EXPECT_EQ(5, A.GetN(5));
+    // Assert
+    EXPECT_EQ(5, A.GetN(5));
 }
 
 TEST(Levitsky_Ilya_setTest, Disjunctio_Sets) {
-  // Arrange
-  set A;
-  set B;
-  set C;
+    // Arrange
+    set A;
+    set B;
+    set C;
 
-  A.Insert(4);
-  B.Insert(7);
+    // Act
+    A.Insert(4);
+    B.Insert(7);
 
-  // Act
-  C = A + B;
+    C = A + B;
 
-  // Assert
-  EXPECT_EQ(4, C.GetN(4));
-  EXPECT_EQ(7, C.GetN(7));
+    // Assert
+    EXPECT_EQ(4, C.GetN(4));
+    EXPECT_EQ(7, C.GetN(7));
 }
 
 TEST(Levitsky_Ilya_setTest, Conjuctio_Sets) {
-  // Arrange
-  set A;
-  set B;
-  set C;
+    // Arrange
+    set A;
+    set B;
+    set C;
 
-  A.Insert(2);
-  A.Insert(4);
-  A.Insert(6);
+    // Act
+    A.Insert(2);
+    A.Insert(4);
+    A.Insert(6);
 
-  B.Insert(3);
-  B.Insert(4);
-  B.Insert(9);
-  B.Insert(6);
+    B.Insert(3);
+    B.Insert(4);
+    B.Insert(9);
+    B.Insert(6);
 
-  // Act
-  C = A*B;
+    C = A*B;
 
-  // Assert
-  EXPECT_EQ(4, C.GetN(4));
-  EXPECT_EQ(6, C.GetN(6));
+    // Assert
+    EXPECT_EQ(4, C.GetN(4));
+    EXPECT_EQ(6, C.GetN(6));
 }
 
 TEST(Levitsky_Ilya_setTest, Addition_Set) {
-  // Arrange
-  set A;
-  set C;
+    // Arrange
+    set A;
+    set C;
 
-  A.Insert(5);
-  A.Insert(4);
-  A.Insert(6);
-  A.Insert(3);
-  A.Insert(2);
+    // Act
+    A.Insert(5);
+    A.Insert(4);
+    A.Insert(6);
+    A.Insert(3);
+    A.Insert(2);
 
-  // Act
-  C = ~A;
+    C = ~A;
 
-  // Assert
-  EXPECT_EQ(1, C.GetN(1));
-  EXPECT_EQ(7, C.GetN(7));
-  EXPECT_EQ(8, C.GetN(8));
-  EXPECT_EQ(9, C.GetN(9));
-  EXPECT_EQ(10, C.GetN(10));
+    // Assert
+    EXPECT_EQ(1, C.GetN(1));
+    EXPECT_EQ(7, C.GetN(7));
+    EXPECT_EQ(8, C.GetN(8));
+    EXPECT_EQ(9, C.GetN(9));
+    EXPECT_EQ(10, C.GetN(10));
 }
 
 TEST(Levitsky_Ilya_setTest, Can_Get_Bit) {
@@ -123,11 +123,11 @@ TEST(Levitsky_Ilya_setTest, Dif_Disjunctio_Sets) {
     set B;
     set C;
 
+	// Act
     A.Insert(4);
     B.Insert(7);
     B.Insert(5);
 
-    // Act
     C = A + B;
 
     // Assert
