4c5990f6f1a9186df91c87e1cd9e35b9ba7b5ec6
Alex Kozorez <heriou@yandex.ru>
2018-05-21 19:41:39 +0300

    im done

--- modules/books/src/Books-app.cpp
+++ modules/books/src/Books-app.cpp
@@ -12,66 +12,66 @@ BooksApplication::BooksApplication() : message_("") {}
 
 
 void BooksApplication::Help(const char * appname, const char * message) {
-	message_ =
-		std::string(message) +
-		"This is a books application.\n\n" +
-		"Please provide arguments in the following format:\n\n" +
-		"  $ " + appname + "<book1> <book2> <book3> <book4> <book5> " +
-		"<output_file_name\n\n" +
-		"Books counts should be in non-negative integer numbers " +
-		"<output_file_name> is the filename where you want to save result.\n";
+    message_ =
+        std::string(message) +
+        "This is a books application.\n\n" +
+        "Please provide arguments in the following format:\n\n" +
+        "  $ " + appname + "<book1> <book2> <book3> <book4> <book5> " +
+        "<output_file_name\n\n" +
+        "Books counts should be in non-negative integer numbers " +
+        "<output_file_name> is the filename where you want to save result.\n";
 }
 
 bool BooksApplication::ValidateNumberOfArguments(int argc, const char ** argv) {
-	if (argc == 1) {
-		Help(argv[0]);
-		return false;
-	}
-	else if (argc != 7) {
-		Help(argv[0], "ERROR: Should be 6 arguments.\n\n");
-		return false;
-	}
-	return true;
+    if (argc == 1) {
+        Help(argv[0]);
+        return false;
+    }
+    else if (argc != 7) {
+        Help(argv[0], "ERROR: Should be 6 arguments.\n\n");
+        return false;
+    }
+    return true;
 }
 
 int parseInt(const char* arg) {
-	int value = std::stoi(std::string(arg));
-	return value;
+    int value = std::stoi(std::string(arg));
+    return value;
 }
 
-std::string BooksApplication::operator()(int argc, const char ** argv)
-{
-	Arguments args;
+std::string BooksApplication::operator()(int argc, const char ** argv) {
+    Arguments args;
 
-	if (!ValidateNumberOfArguments(argc, argv)) return message_;
+    if (!ValidateNumberOfArguments(argc, argv)) return message_;
 
-	try {
-		args.books = new int[5];
-		for (int i = 0; i < 5; i++)
-			args.books[i] = parseInt(argv[i+1]);
-		args.output_file_name = argv[6];
-	}
-	catch (std::exception& exc) {
-		return std::string("Wrong format or value is out of range");
-	}
+    try {
+        args.books = new int[5];
+        for (int i = 0; i < 5; i++)
+            args.books[i] = parseInt(argv[i+1]);
+        args.output_file_name = argv[6];
+    }
+    catch (std::exception& exc) {
+        return std::string("Wrong format or value is out of range");
+    }
 
-	try {
-		Books books_area(5, args.books);
-		std::ostringstream stream;
-		std::ofstream output_file_stream(args.output_file_name);
+    try {
+        Books books_area(5, args.books);
+        std::ostringstream stream;
+        std::ofstream output_file_stream(args.output_file_name);
 
-		if (output_file_stream.is_open()) {
-			output_file_stream << books_area.CalculateAllPayment(args.books);
-			output_file_stream.close();
+        if (output_file_stream.is_open()) {
+            output_file_stream << books_area.CalculateAllPayment(args.books);
+            output_file_stream.close();
 
-			stream << "Successfully written to " << args.output_file_name;
-		}
-		else stream << "Cannot open " << args.output_file_name;
+            stream << "Successfully written to " << args.output_file_name;
+        }
+        else 
+            stream << "Cannot open " << args.output_file_name;
 
-		message_ = stream.str();
-		return message_;
-	}
-	catch (const char* exc) {
-		return std::string(exc);
-	}
+        message_ = stream.str();
+        return message_;
+    }
+    catch (const char* exc) {
+        return std::string(exc);
+    }
 }
--- modules/books/test/test_books_application.cpp
+++ modules/books/test/test_books_application.cpp
@@ -15,77 +15,79 @@ using std::vector;
 using std::string;
 
 class BooksApplicationTest : public ::testing::Test {
-protected:
-	// virtual void SetUp() {}
+ protected:
+    // virtual void SetUp() {}
 
-	void Act(vector<string> args_) {
-		vector<const char*> options;
+    void Act(vector<string> args_) {
+        vector<const char*> options;
 
-		options.push_back("appname");
-		for (size_t i = 0; i < args_.size(); ++i) {
-			options.push_back(args_[i].c_str());
-		}
+        options.push_back("appname");
+        for (size_t i = 0; i < args_.size(); ++i) {
+            options.push_back(args_[i].c_str());
+        }
 
-		const char** argv = &options.front();
-		int argc = static_cast<int>(args_.size()) + 1;
+        const char** argv = &options.front();
+        int argc = static_cast<int>(args_.size()) + 1;
 
-		output_ = app_(argc, argv);
-	}
+        output_ = app_(argc, argv);
+    }
 
-	void Assert(std::string expected) {
-		EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
-	}
+    void Assert(std::string expected) {
+        EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+    }
 
-private:
-	BooksApplication app_;
-	string output_;
+ private:
+    BooksApplication app_;
+    string output_;
 };
 
 TEST_F(BooksApplicationTest, Do_Print_Help_Without_Arguments) {
-	vector<string> args = {};
+    vector<string> args = {};
 
-	Act(args);
+    Act(args);
 
-	Assert("This is a books application\\..*");
+    Assert("This is a books application\\..*");
 }
 
 TEST_F(BooksApplicationTest, Is_Checking_Number_Of_Arguments) {
-	vector<string> args = { "1","2","95" };
+    vector<string> args = { "1", "2", "95" };
 
-	Act(args);
+    Act(args);
 
-	Assert("ERROR: Should be 6 arguments\\..*");
+    Assert("ERROR: Should be 6 arguments\\..*");
 }
 
 TEST_F(BooksApplicationTest, Can_Detect_Wrong_Value_Format) {
-	vector<string> args = { "a", "2", "4", "5", "6", "out.txt" };
+    vector<string> args = { "a", "2", "4", "5", "6", "out.txt" };
 
-	Act(args);
+    Act(args);
 
-	Assert("Wrong format or value is out of range");
+    Assert("Wrong format or value is out of range");
 }
 
 TEST_F(BooksApplicationTest, Can_Detect_Wrong_Operation_Format) {
-	vector<string> args = { "99999999999999", "999999999999999", "99999999999999", "999999999999999", "99999999999999", "out.txt" };
+    vector<string> args = { "99999999999999", "999999999999999",
+    "99999999999999",
+    "999999999999999", "99999999999999", "out.txt" };
 
-	Act(args);
+    Act(args);
 
-	Assert("Wrong format or value is out of range");
+    Assert("Wrong format or value is out of range");
 }
 
 TEST_F(BooksApplicationTest, Can_Save_Found_Numbers_To_File) {
-	vector<string> args = { "1", "2", "2", "2", "2", "out.txt" };
+    vector<string> args = { "1", "2", "2", "2", "2", "out.txt" };
 
-	Act(args);
+    Act(args);
 
-	Assert("Successfully written to.*");
+    Assert("Successfully written to.*");
 }
 
 
 TEST_F(BooksApplicationTest, Can_Print_Error_If_File_Cannot_Be_Opened) {
-	vector<string> args = { "1", "2", "2", "2", "2", "Nonexistent:\\/" };
+    vector<string> args = { "1", "2", "2", "2", "2", "Nonexistent:\\/" };
 
-	Act(args);
+    Act(args);
 
-	Assert("Cannot open.*");
+    Assert("Cannot open.*");
 }
