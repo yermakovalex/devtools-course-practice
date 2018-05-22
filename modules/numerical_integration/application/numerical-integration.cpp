aab6a5e25bd48bc4dc6c2703d7d67aeea1b0d0b1
Куликова Светлана Александровна <37101627+SylaiseElvenan@users.noreply.github.com>
2018-05-22 14:17:38 +0300

    Create numerical-integration.cpp

---
+++ modules/numerical_integration/application/numerical-integration.cpp
@@ -0,0 +1,14 @@
+// Copyright 2017 Kulikova Svetlana
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+// #include "include/numerical-integration-app.h"
+
+int main(int argc, const char** argv) {
+    // IntegralApp app;
+    std::string output = "Hello world"; // app(argc, argv);
+    printf("%s\n", output.c_str());
+    return 0;
+}
