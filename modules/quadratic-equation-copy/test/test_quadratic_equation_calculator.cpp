ed32179fefae55f042db8c96823051472b15963f
x-ndr <al.romanov2012@yandex.ru>
2018-05-25 00:38:45 +0300

    added template for lab3

---
+++ modules/quadratic-equation-copy/test/test_quadratic_equation_calculator.cpp
@@ -0,0 +1,14 @@
+// Copyright 2018 Romanov Alexander
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "include/quadratic_equation.h"
+#include "include/quadratic_equation_calculator.h"
+
+TEST(Romanov_Alexander_QuadraticEquationCalculatorTest,
+     SmokeTest) {
+    // AAA
+    ASSERT_TRUE(1 == 1)	;
+}
