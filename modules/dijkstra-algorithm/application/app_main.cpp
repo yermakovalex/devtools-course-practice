121aaad08eeca9724ebbec99cb23ed0faf33da47
Lagerta <enniluisarita@gmail.com>
2018-05-25 12:50:26 +0300

    Inital commit

---
+++ modules/dijkstra-algorithm/application/app_main.cpp
@@ -0,0 +1,10 @@
+// Copyright 2018 Bogatova Margarita
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+int main(int argc, const char** argv) {
+	printf("Heloo World!\n");
+	return 0;
+}
