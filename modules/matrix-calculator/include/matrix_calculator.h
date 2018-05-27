9d5fb60755f544796c92cf0ec2dab3ce606839a3
SolarSystem23 <piccione-mail@yandex.ru>
2018-05-27 20:20:40 +0300

    refactoring

--- modules/matrix-calculator/include/matrix_calculator.h
+++ modules/matrix-calculator/include/matrix_calculator.h
@@ -2,14 +2,14 @@
 
 #ifndef MODULES_MATRIX_CALCULATOR_INCLUDE_MATRIX_CALCULATOR_H_
 #define MODULES_MATRIX_CALCULATOR_INCLUDE_MATRIX_CALCULATOR_H_
-
+#include <iostream>
 #include <vector>
 using matrix_type = std::vector<std::vector<double>>;
 
 class MatrixCalculator {
  public:
     MatrixCalculator();
-    MatrixCalculator(const int rows, const int columns);
+    MatrixCalculator(int rows, int columns);
     MatrixCalculator(const MatrixCalculator&);
 
     void SetMatrix(matrix_type&);
@@ -24,9 +24,13 @@ class MatrixCalculator {
 
     bool operator==(const MatrixCalculator&) const;
     bool AreSizesEqual(const MatrixCalculator&) const;
-    matrix_type matrix;
     MatrixCalculator Prepare_for_minores(int, matrix_type, int, int) const;
     MatrixCalculator Transpon(int a) const;
+
+    friend std::ostream& operator<<(std::ostream& stream,
+    	                       const MatrixCalculator& result);
+private:
+    matrix_type matrix;
 };
 
 #endif  // MODULES_MATRIX_CALCULATOR_INCLUDE_MATRIX_CALCULATOR_H_
