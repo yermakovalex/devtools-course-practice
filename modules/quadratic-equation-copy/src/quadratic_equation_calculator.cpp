ef17ac801152a31e5a7fee36cfa687cced5d1879
Alexander Romanov <al.romanov2012@yandex.ru>
2018-05-27 13:12:33 +0300

    deleted dir

--- modules/quadratic-equation-copy/src/quadratic_equation_calculator.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 Romanov Alexander
-
-#include "include/quadratic_equation_calculator.h"
-#include "include/quadratic_equation.h"
-
-#include <math.h>
-#include <stdexcept>
-#include <vector>
