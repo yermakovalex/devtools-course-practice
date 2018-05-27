64dd88bf92fc006c5aad081a3ae5c1fb5473c878
Арсений Маячкин <maackiarsenij@MacBook-Pro-Arsenij.local>
2018-05-27 15:52:31 +0300

    Update file.

--- modules/batcher-merge/src/batcher_merge.cpp
+++ modules/batcher-merge/src/batcher_merge.cpp
@@ -14,9 +14,9 @@ void compexch(int& a, int& b)
 
 void shuffle(std::vector<int>& a, unsigned int l, unsigned int r)
 {
-   auto half = (unsigned int)(l + r) / 2;
+   int half = (unsigned int)(l + r) / 2;
    std::vector<int> tmp(a.size());
-   unsigned int i, j;
+   int i, j;
 
    for (i = l, j = 0; i <= r; i += 2, j++)
    {
@@ -24,15 +24,15 @@ void shuffle(std::vector<int>& a, unsigned int l, unsigned int r)
       tmp[i + 1] = a[half + j + 1];
    }
     
-   for (auto i = 0; i < tmp.size(); i++)
+   for (int i = 0; i < tmp.size(); i++)
       a[i] = tmp[i];
 }
 
 void unshuffle(std::vector<int>& a, unsigned int l, unsigned int r)
 {
-   auto half = (unsigned int)(l + r) / 2;
+   int half = (unsigned int)(l + r) / 2;
    std::vector<int> tmp(a.size());
-   unsigned int i, j;
+   int i, j;
     
    for (i = l, j = 0; i <= r; i += 2, j++)
    {
@@ -40,7 +40,7 @@ void unshuffle(std::vector<int>& a, unsigned int l, unsigned int r)
       tmp[half + j + 1] = a[i + 1];
    }
     
-   for (auto i = 0; i < tmp.size(); i++)
+   for (int i = 0; i < tmp.size(); i++)
       a[i] = tmp[i];
 }
 
@@ -56,16 +56,16 @@ void merge::BatcherMerge(std::vector<int>& a, unsigned int l, unsigned int r)
       return;
     
    unshuffle(a, l, r);
-   auto half = (unsigned int)(l + r) / 2;
+   int half = (int)(l + r) / 2;
     
-   OddEvenMergeSort(a, l, half);
-   OddEvenMergeSort(a, half + 1, r);
+   BatcherMerge(a, l, half);
+   batcherMerge(a, half + 1, r);
     
    shuffle(a, l, r);
-   for (auto i = l + 1; i < r; i += 2)
+   for (int i = l + 1; i < r; i += 2)
       compexch(a[i], a[i + 1]);
 
-   auto halfSize = (r - l + 1) / 2 - 1;
+   int halfSize = (r - l + 1) / 2 - 1;
 	
    for (int i = l + 1; i + halfSize < r; i++)
       compexch(a[i], a[i + halfSize]);
