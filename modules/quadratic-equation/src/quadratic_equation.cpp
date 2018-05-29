cfb067c8f6b4e629b47af959bcc37eb3bc30787b
Daniil <haxaji2012@gmail.com>
2018-05-29 21:28:47 +0300

    Fixed #4

--- modules/quadratic-equation/src/quadratic_equation.cpp
+++ modules/quadratic-equation/src/quadratic_equation.cpp
@@ -18,9 +18,11 @@ std::vector<double> QuadraticEquation::getRoots() {
     if (isSolved) {
         if (this->hasRealRoots())
             return roots;
-        else
-            throw std::runtime_error(
-                  "Quadratic equation has no real roots");
+        else {
+            /*throw std::runtime_error(
+                  "Quadratic equation has no real roots");*/
+            throw "Quadratic equation has no real roots";
+		}
     } else {
         this->solve();
         return this->getRoots();
@@ -31,8 +33,9 @@ void QuadraticEquation::setCoefficients(const double& _a,
                                         const double& _b,
                                         const double& _c) {
     if (_a == 0.f)
-        throw std::invalid_argument(
-            "Quadratic coefficient can not be equal zero");
+	    throw "Quadratic coefficient can not be equal zero";
+        /*throw std::invalid_argument(
+            "Quadratic coefficient can not be equal zero");*/
     a = _a; b = _b; c = _c;
     discriminant = b*b - 4 * a*c;
     isSolved = false;
