67273ab0d798ceca75ccbab5c6ffbf18a2d9266d
GoodQeu <slalik.lenkin@gmail.com>
2018-05-29 22:23:57 +0300

    v6

--- modules/caesar-cipher/src/Caesar_Cipher.cpp
+++ modules/caesar-cipher/src/Caesar_Cipher.cpp
@@ -1,4 +1,6 @@
-#include "Caesar_Cipher.h"
+// Copyright 2018 Lenkin Slava
+#include "include/Caesar_Cipher.h"
+#include <string>
 
 std::string CAESAR_CIPHER::cipher(const std::string & input_s, const int shift){
     bool Ok;
@@ -17,14 +19,18 @@ std::string CAESAR_CIPHER::cipher(const std::string & input_s, const int shift){
              }
              else if (input_s[i] == high_ch[j]){
                 j += shift;
-                while (j >= ABCSize) j -= ABCSize;
-                while (j < 0) j += ABCSize;
-                output_s += high_ch[j];
+                while (j >= ABCSize) j -= ABCSize; {
+                    while (j < 0) j += ABCSize; {
+                      output_s += high_ch[j];
+                    }
+				}
                 Ok = true;
                 break;
              }
         }
-		if (!Ok) output_s += input_s[i];
+        if (!Ok){
+			output_s += input_s[i];
+        }
     }
       return output_s;
 }
@@ -43,10 +49,14 @@ void CAESAR_CIPHER::Init_symbols(){
     this->ABCSize = 26;
     this->high_ch = new char[ABCSize];
     this->low_ch = new char[ABCSize];
-    const char low_ch[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-    const char high_ch[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    const char low_ch[] = { 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z' };
+    const char high_ch[] = { 'A', 'B', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+        'W', 'X', 'Y', 'Z' };
     for (int i = 0; i < ABCSize; i++){
         this->high_ch[i] = high_ch[i];
         this->low_ch[i] = low_ch[i];
