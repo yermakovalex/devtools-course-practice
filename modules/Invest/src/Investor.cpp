7b0b2068745967dba06d445e5abc0c41f235e28f
unknown <v02055020v@gmail.com>
2018-05-29 16:47:35 +0300

    edited tab 3

--- modules/Invest/src/Investor.cpp
+++ modules/Invest/src/Investor.cpp
@@ -1,6 +1,4 @@
 // Copyright 2018 Chernenko Valery
-#include "include/Invest.h"
-#include "include/Investor.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +6,9 @@
 #include <string.h>
 #include <string>
 #include <sstream>
+
+#include "include/Invest.h"
+#include "include/Investor.h"
 
 Investor::Investor() : message_("") {}
 
@@ -17,20 +18,20 @@ void Investor::help(const char* appname, const char* message) {
         "Please provide arguments in the following format:\n\n" +
 
         "  $ " + appname + " <int YearCount> <money* Incomes> " +
-        "<money* Expenses> <double RatePercent> <char* Function> [double RateParameter]\n\n";
+        "<money* Expenses> <double RatePercent>
+        <char* Function> [double RateParameter]\n\n";
 }
 
-bool Investor::validateNumberOfArguments(int argc,const char** argv) {
+bool Investor::validateNumberOfArguments(int argc, const char** argv) {
     if (argc == 1) {
         help(argv[0]);
         return false;
-    }
-    else {
+    } else {
         int year = atoi(argv[1]);
-        if (argc == 4 + year * 2 || (argc == 5 + year * 2 && !strcmp(argv[2 + year * 2 + 1],"FindNPV"))) {
+        if (argc == 4 + year * 2 || 
+           (argc == 5 + year * 2 && !strcmp(argv[2 + year * 2 + 1], "FindNPV"))) {
             return true;
-        }
-        else {
+        } else {
             help(argv[0], "Wrong Parameters!\n");
             return false;
         }
@@ -57,41 +58,42 @@ std::string Investor::operator()(int argc, const char** argv) {
     try {
         args.YearCount = atoi(argv[1]);
         args.Incomes.reserve(args.YearCount);
-        for(int i = 0; i < args.YearCount; i++)
-            args.Incomes.push_back (parseDouble(argv[2+i]));
+        for (int i = 0; i < args.YearCount; i++)
+            args.Incomes.push_back(argv[2+i]));
         args.Expenses.reserve(args.YearCount);
         for (int i = 0; i < args.YearCount; i++)
             args.Expenses.push_back(parseDouble(argv[2 +args.YearCount + i]));
         args.RatePercent = parseDouble(argv[2 + args.YearCount*2]);
         args.Function = new char[30];
-        strcpy (args.Function,argv[2 + args.YearCount * 2 + 1]);
-        if (strcmp(args.Function, "FindNPV") != 0 && strcmp(args.Function, "FindIRR") != 0 &&
-            strcmp(args.Function, "FindRecoupmentTime") != 0 && strcmp(args.Function, "FindPBIndex") != 0) 
+        strcpy(args.Function, argv[2 + args.YearCount * 2 + 1]);
+        if (strcmp(args.Function, "FindNPV") != 0 && 
+            strcmp(args.Function, "FindIRR") != 0 &&
+            strcmp(args.Function, "FindRecoupmentTime") != 0 && 
+            strcmp(args.Function, "FindPBIndex") != 0)
             throw std::string("Wrong Operation!\n");
-        if (strcmp(args.Function, "FindNPV") ==  0 && argc == 5 + args.YearCount * 2)
+        if (strcmp(args.Function, "FindNPV") ==  0 && 
+            argc == 5 + args.YearCount * 2)
             args.RateParameter = parseDouble(argv[argc-1]);
-        else args.RateParameter = -1;
+        else 
+            args.RateParameter = -1;
     }
     catch (std::string& str) {
         return str;
     }
 
-    Investition invest(args.YearCount, args.Incomes, args.Expenses, args.RatePercent);
+    Investition invest(args.YearCount, args.Incomes, 
+                       args.Expenses, args.RatePercent);
     std::ostringstream stream;
-    if (!strcmp(args.Function, "FindNPV") && argc == 5 + args.YearCount * 2)
-    {
+    if (!strcmp(args.Function, "FindNPV") && 
+                argc == 5 + args.YearCount * 2) {
         stream << "NPV = " << invest.FindNPV(args.RateParameter);
-    }
-    else if (!strcmp(args.Function, "FindNPV")) {
+    } else if (!strcmp(args.Function, "FindNPV")) {
         stream << "NPV = " << invest.FindNPV();
-    }
-    else if (!strcmp(args.Function, "FindIRR")) {
+    } else if (!strcmp(args.Function, "FindIRR")) {
         stream << "IRR = " << invest.FindIRR();
-    }
-    else if (!strcmp(args.Function, "FindRecoupmentTime")) {
+    } else if (!strcmp(args.Function, "FindRecoupmentTime")) {
         stream << "RecoupmentTime = " << invest.FindRecoupmentTime();
-    }
-    else {
+    } else {
         stream << "PBIndex = " << invest.FindPBIndex();
     }
 
