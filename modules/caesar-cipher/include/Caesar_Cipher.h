b83f376d6c882af3bcedcafb4e35b007731667e0
GoodQeu <slalik.lenkin@gmail.com>
2018-05-29 21:45:16 +0300

    v3

--- modules/caesar-cipher/include/Caesar_Cipher.h
+++ modules/caesar-cipher/include/Caesar_Cipher.h
@@ -1,4 +1,4 @@
-//Copyright 2018 Lenkin Slava
+// Copyright 2018 Lenkin Slava
 #ifndef MODULES_CAESAR_CIPHER_INCLUDE_CAESAR_CIPHER_H_
 #define MODULES_CAESAR_CIPHER_INCLUDE_CAESAR_CIPHER_H_
 
@@ -6,16 +6,16 @@
 #include <string>
 
 class CAESAR_CIPHER{
-public:
+ public:
     std::string cipher(const std::string & input_s, const int shift);
     int ABCSize;
     CAESAR_CIPHER();
     ~CAESAR_CIPHER();
-private:	
+ private:	
     char *low_ch;
     char *high_ch;
     void Init_symbols();
 };
 
 
-#endif  //MODULES_CAESAR_CIPHER_INCLUDE_CAESAR_CIPHER_H_
+#endif  // MODULES_CAESAR_CIPHER_INCLUDE_CAESAR_CIPHER_H_
