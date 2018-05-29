64a85c54a19ebc0244f088460b82902cfd14881e
GusevAlexandr <gusevsasha1996@mail.ru>
2018-05-29 18:36:00 +0300

    try

---
+++ modules/area_calculation/src/area_calculation_app.cpp
@@ -0,0 +1,70 @@
+// Copyright 2018 Gusev Alexandr
+
+#include "include/area_calculation.h"
+#include "include/area_calculation_app.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <string>
+#include <sstream>
+
+AreaCalculationApp::AreaCalculationApp() : message_("") {}
+
+void AreaCalculationApp::help(const char* appname, const char* message) {
+	message_ =
+		std::string(message) +
+		"This is a area calculation application.\n\n" +
+		"Please provide arguments in the following format:\n\n" +
+		"  $ " + appname + " <int>size \n\n" +
+		" + x[i] + y[i] \n\n" +
+		"Where all arguments are double numbers and 0 < i < size .\n";
+}
+
+bool AreaCalculationApp::validateNumberOfArguments(int argc,
+    const char** argv) {
+    if (argc == 1) {
+        help(argv[0]);
+        return false;
+    }
+    else if (argc != atoi(argv[1]) * 2 + 2) {
+        help(argv[0], "ERROR: Should be 2 * size + 1 arguments.\n\n");
+        return false;
+    }
+    return true;
+}
+
+double parseDouble(const char* arg) {
+    char* end;
+    double value = strtod(arg, &end);
+    if (end[0]) {
+        throw std::string("Wrong number format!");
+    }
+    return value;
+}
+
+std::string AreaCalculationApp::operator()(int argc, const char** argv) {
+    Arguments args;
+    if (!validateNumberOfArguments(argc, argv)) {
+        return message_;
+    }
+    try {
+        args.size = atoi(argv[1]);
+        args.d1 = new double[args.size];
+        args.d2 = new double[args.size];
+        for (int i = 0; i < args.size; i++)
+        {
+            args.d1[i] = parseDouble(argv[2*i]);
+            args.d2[i] = parseDouble(argv[2 * i + 1]);
+        }
+    }
+    catch (std::string& str) {
+        return str;
+    }
+    AreaCalculation ac(args.size, args.d1, args.d2);
+    std::ostringstream stream;
+    stream << "area = " << ac.GetArea();
+    message_ = stream.str();
+    return message_;
+}
---
+++ modules/area_calculation/test/test_gusev_alexandr_area_calculation_app.cpp
@@ -0,0 +1,51 @@
+// Copyright 2018 Gusev Alexandr
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+#include "include/area_calculation_app.h"
+//#include "include/area_calculation.h"
+using ::testing::internal::RE;
+using std::vector;
+using std::string;
+
+class AreaCalculationAppTest : public ::testing::Test {
+ protected:
+	// virtual void SetUp() {}
+
+	void Act(vector<string> args_) {
+		vector<const char*> options;
+
+		options.push_back("appname");
+		for (size_t i = 0; i < args_.size(); ++i) {
+			options.push_back(args_[i].c_str());
+		}
+
+		const char** argv = &options.front();
+		int argc = static_cast<int>(args_.size()) + 1;
+
+		output_ = app_(argc, argv);
+	}
+
+	void Assert(std::string expected) {
+		EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+	}
+
+private:
+	AreaCalculationApp app_;
+	string output_;
+};
+
+TEST_F(AreaCalculationAppTest, No_Arguments_Test) {
+    // Arrange
+    vector<string> args = {};
+    // Act
+	Act(args);
+    // Assert
+	Assert("This is a area calculation application\\..*");
+}
