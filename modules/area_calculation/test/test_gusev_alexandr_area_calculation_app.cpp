01f480be8df53e4f0378a37454a688614a60dbe0
GusevAlexandr <gusevsasha1996@mail.ru>
2018-05-30 11:15:29 +0300

    fix

--- modules/area_calculation/test/test_gusev_alexandr_area_calculation_app.cpp
+++ modules/area_calculation/test/test_gusev_alexandr_area_calculation_app.cpp
@@ -51,7 +51,7 @@ TEST_F(AreaCalculationAppTest, No_Arguments_Test) {
 }
 TEST_F(AreaCalculationAppTest, Correctly_Arguments_Test) {
 	// Arrange
-	vector<string> args = { "3", "1", "1", "1", "1", "2", "1"};
+	vector<string> args = { "3", "1.0", "1.0", "1.0", "1.0", "2.0", "1.0"};
 	// Act
 	Act(args);
 	// Assert
