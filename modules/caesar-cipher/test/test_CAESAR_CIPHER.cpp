bc65ee605b990bd1ab60087f2a15634255ac9619
GoodQeu <slalik.lenkin@gmail.com>
2018-05-30 16:45:41 +0300

    add test

--- modules/caesar-cipher/test/test_CAESAR_CIPHER.cpp
+++ modules/caesar-cipher/test/test_CAESAR_CIPHER.cpp
@@ -80,3 +80,23 @@ TEST(CAESAR_CIPHER_Test, Can_Convert7) {
     // Assert
     EXPECT_EQ("cccddd", result);
 }
+TEST(CAESAR_CIPHER_Test, Can_Convert8) {
+	// Arrage
+	std::string input_s = "AA";
+	int shift = 1;
+	CAESAR_CIPHER tmp;
+	// Act
+	std::string result = tmp.cipher(input_s, shift);
+	// Assert
+	EXPECT_EQ("BB", result);
+}
+TEST(CAESAR_CIPHER_Test, Can_Convert9) {
+	// Arrage
+	std::string input_s = "AA BB";
+	int shift = 1;
+	CAESAR_CIPHER tmp;
+	// Act
+	std::string result = tmp.cipher(input_s, shift);
+	// Assert
+	EXPECT_EQ("BB CC", result);
+}
