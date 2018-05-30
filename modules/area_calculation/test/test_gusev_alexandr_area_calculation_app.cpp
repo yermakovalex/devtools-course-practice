4a648f566bcffdff998badfe1ef66dac87bb2ee1
GusevAlexandr <gusevsasha1996@mail.ru>
2018-05-30 15:55:37 +0300

    fix

--- modules/area_calculation/test/test_gusev_alexandr_area_calculation_app.cpp
+++ modules/area_calculation/test/test_gusev_alexandr_area_calculation_app.cpp
@@ -57,10 +57,10 @@ TEST_F(AreaCalculationAppTest, No_Arguments_Test) {
 	Assert("Wrong number format!");
 }*/
 TEST_F(AreaCalculationAppTest, Correctly_Area_Calculation_Test) {
-	// Arrange
-	vector<string> args = { "3", "1.0", "2.0", "3.0", "1.0", "2.0", "1.0" };
-	// Act
-	Act(args);
-	// Assert
-	Assert("area = 1");
+    // Arrange
+    vector<string> args = { "3", "1.0", "2.0", "3.0", "1.0", "2.0", "1.0" };
+    // Act
+    Act(args);
+    // Assert
+    Assert("area = 1");
 }
