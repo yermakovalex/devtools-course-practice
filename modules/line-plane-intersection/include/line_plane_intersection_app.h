63853b07b316045bdabe119e0bafb6051cfdc2f0
Lvova Alina <37267494+AlinaLvova@users.noreply.github.com>
2018-05-31 02:55:53 +0300

    Add files via upload

---
+++ modules/line-plane-intersection/include/line_plane_intersection_app.h
@@ -0,0 +1,24 @@
+// Copyright 2018 Lvova Alina
+
+#ifndef MODULES_LINE_PLANE_INTERSECTION_INCLUDE_LINE_PLANE_INTERSECTION_APP_H_
+#define MODULES_LINE_PLANE_INTERSECTION_INCLUDE_LINE_PLANE_INTERSECTION_APP_H_
+
+#include <string>
+#include "C:\Users\Лена\Downloads\devtools-course-practice-master\devtools-course-practice-master\modules\line-plane-intersection\include\line_plane_intersection.h"
+
+class LinePlaneIntersectionApp {
+private:
+    std::string message_;
+    void Help(const char* appname, const char* message = "");
+    bool ValidateNumberOfArguments(int argc, const char** argv);
+    typedef struct {
+        std::string operation;
+        Dot* plane;
+        Dot* line;
+    } Arguments;
+public:
+    LinePlaneIntersectionApp();
+    std::string operator()(int argc, const char** argv);
+};
+
+#endif  // MODULES_LINE_PLANE_INTERSECTION_INCLUDE_LINE_PLANE_INTERSECTION_APP_H_
