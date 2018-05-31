f6b6e4f656d5e92e1ed5428d2a0383f540e81386
KseniaGorelova <32457353+KseniaGorelova@users.noreply.github.com>
2018-05-31 22:29:09 +0300

    Update test_binary_search_app.cpp

--- modules/binary-search/test/test_binary_search_app.cpp
+++ modules/binary-search/test/test_binary_search_app.cpp
@@ -12,7 +12,7 @@ using std::vector;
 using std::string;
 
 class BinarySearchAppTest : public ::testing::Test {
-protected:
+ protected:
     void Act(vector<string> args_) {
         vector<const char*> options;
 
@@ -31,7 +31,7 @@ protected:
         EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
     }
 
-private:
+ private:
     BinarySearchApp app_;
     string output_;
 };
