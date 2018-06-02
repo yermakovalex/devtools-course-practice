e867fb3281fbdf8296744f68d8459251e73f0b59
mikkon98 <vyunov.rom@mail.ru>
2018-06-02 12:48:15 +0300

    Simple regex header finished

--- modules/simple-regex/include/simple_regex-app.h
+++ modules/simple-regex/include/simple_regex-app.h
@@ -1,7 +1,25 @@
 // Copyright 2018 Vyunov Roman
 
-#include "simple_regex.h"
+#ifndef MODULES_SIMPLE_REGEX_INCLUDE_SIMPLE_REGEX_APP_H_
+#define MODULES_SIMPLE_REGEX_INCLUDE_SIMPLE_REGEX_APP_H_
+
+#include <string>
 
 class Application {
+public:
+    Application();
+    std::string operator()(int argc, const char** argv);
 
+private:
+    void help(const char* appname, const char* message = "");
+    bool validateNumberOfArguments(int argc, const char** argv);
+    std::string message_;
+    struct
+    {
+        std::string mode;
+        std::string target;
+        std::string regex;
+    } Arguments;
 };
+
+#endif  // MODULES_SIMPLE_REGEX_INCLUDE_SIMPLE_REGEX_APP_H_
