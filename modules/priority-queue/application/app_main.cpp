c08d968b2d46499bb4097e87780f02a2ada79fa8
Imadzuma <mar-gosha98@mail.ru>
2018-06-02 21:38:48 +0300

    app_main.cpp

--- modules/priority-queue/application/app_main.cpp
+++ modules/priority-queue/application/app_main.cpp
@@ -1,12 +1,12 @@
+// Copyright 2018 Mezina Margarita
+
 #include <iostream>
 #include <string>
 #include "include/priority_queue_app.h"
 
-using namespace std;
-
 int main(int argc, const char** argv) {
-	PriorityQueueApp app;
-	std::string output = app(argc, argv);
-	std::cout << output.c_str() << std::endl;
-	return 0;
+    PriorityQueueApp app;
+    std::string output = app(argc, argv);
+    std::cout << output.c_str() << std::endl;
+    return 0;
 }
