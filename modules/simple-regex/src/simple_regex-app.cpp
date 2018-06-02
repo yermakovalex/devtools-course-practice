0934b538aee5ac20998cb25b1b2b318914d43a59
mikkon98 <vyunov.rom@mail.ru>
2018-06-02 17:20:21 +0300

    Some changes in syntax & architecture

--- modules/simple-regex/src/simple_regex-app.cpp
+++ modules/simple-regex/src/simple_regex-app.cpp
@@ -72,7 +72,7 @@ std::string Application::operator()(int argc, const char** argv) {
     case 'g':
         regex.isMatch(args.target.c_str());
         char * tmp = new char[args.target.size()];
-        snprintf(tmp, args.target.size() - 1, args.target.c_str());
+        strcpy_s(tmp, args.target.size(), args.target.c_str());
         regex.getMatch(tmp, args.target.size());
         if (regex.getMatchIndex() != -1) {
             stream << "Find : " << std::string(tmp) << std::endl <<
