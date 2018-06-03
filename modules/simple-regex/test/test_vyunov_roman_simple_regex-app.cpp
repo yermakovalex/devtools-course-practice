4032b20635116fb6584f7fb8c867d984988e1f6a
Roman Vyunov <32357155+mikkon2409@users.noreply.github.com>
2018-06-03 12:22:03 +0300

    Update test_vyunov_roman_simple_regex-app.cpp

--- modules/simple-regex/test/test_vyunov_roman_simple_regex-app.cpp
+++ modules/simple-regex/test/test_vyunov_roman_simple_regex-app.cpp
@@ -15,7 +15,7 @@ using std::vector;
 using std::string;
 
 class SimpleRegexTest : public ::testing::Test {
-protected:
+ protected:
     // virtual void SetUp() {}
 
     void Act(vector<string> args_) {
@@ -36,7 +36,7 @@ protected:
         EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
     }
 
-private:
+ private:
     Application app_;
     string output_;
 };
